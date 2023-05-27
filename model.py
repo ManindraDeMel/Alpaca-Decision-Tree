52627248dafad92b027ca4664c84f45abaebe22a
Manindra de Mel <u7156805@anu.edu.au>
2023-05-27 10:50:09 +1000

    Check market open + finalise a very basic model

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import os
+import time
+from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
@@ -16,67 +17,85 @@ ALPACA_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
 
 api = tradeapi.REST(ALPACA_API_KEY, ALPACA_SECRET_KEY, base_url='https://paper-api.alpaca.markets') 
 
-# Fetch intra-day data (say, for the last 50 days)
-data = api.get_barset('AAPL', 'day', limit=50).df['AAPL']
+def fetch_data(symbol, days):
+    data = api.get_barset(symbol, 'day', limit=days).df[symbol]
 
-# Transform the data to the format similar to what we had before
-df = pd.DataFrame()
-df['Open'] = data['open']
-df['High'] = data['high']
-df['Low'] = data['low']
-df['Close'] = data['close']
+    df = pd.DataFrame()
+    df['Open'] = data['open']
+    df['High'] = data['high']
+    df['Low'] = data['low']
+    df['Close'] = data['close']
 
-# Use shift to create the past 5 days' closing price columns
-for i in range(1, 6):
-    df[f'Close_{i}'] = df['Close'].shift(i)
+    for i in range(1, 6):
+        df[f'Close_{i}'] = df['Close'].shift(i)
 
-# Drop the rows for which we don't have past 5 days' data
-df = df.dropna()
+    df = df.dropna()
 
-# Predict the next day's closing price, so the target column will be the 'Close' column shifted up by 1
-df.loc[:, 'Target'] = df['Close'].shift(-1)
+    df.loc[:, 'Target'] = df['Close'].shift(-1)
+    df = df.dropna()
 
-# Drop the rows for which we don't have target value
-df = df.dropna()
+    return df
 
-# Create the feature matrix (X) and target array (y)
-X = df.drop('Target', axis=1)
-y = df['Target']
+def train_model(X_train, y_train):
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    return model
 
-# Split the data into training set and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+def predict_and_trade(model, X_last, last_close, symbol):
+    next_day_close = model.predict([X_last])
 
-# Create a RandomForestRegressor
-model = RandomForestRegressor(n_estimators=100, random_state=42)
+    if next_day_close > last_close:
+        api.submit_order(
+            symbol=symbol,
+            qty=1,
+            side='buy',
+            type='market',
+            time_in_force='gtc'
+        )
+    else:
+        api.submit_order(
+            symbol=symbol,
+            qty=1,
+            side='sell',
+            type='market',
+            time_in_force='gtc'
+        )
 
-# Train the model
-model.fit(X_train, y_train)
+def is_market_open():
+    # Get the current time in Eastern Time
+    now = datetime.now().astimezone(timezone('US/Eastern'))
 
-# Predict the target values for test set
-y_pred = model.predict(X_test)
+    # Define market open and close hours
+    market_open = now.replace(hour=9, minute=30, second=0, microsecond=0)
+    market_close = now.replace(hour=16, minute=0, second=0, microsecond=0)
 
-# Calculate MAE
-mae = mean_absolute_error(y_test, y_pred)
+    # Check if the current time is within market hours
+    return market_open <= now <= market_close
 
-print(f'Mean Absolute Error: {mae}')
+def main():
+    symbol = 'AAPL'
+    days = 60
+    test_size = 0.2
+    wait_time = 60  # seconds to wait (1 min)
 
-# Use the model to predict the next day's closing price
-next_day_close = model.predict([X.iloc[-1]])
+    while True:
+        if is_market_open():
+            df = fetch_data(symbol, days)
+            X = df.drop('Target', axis=1)
+            y = df['Target']
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
 
-# Place an order based on the prediction
-if next_day_close > df['Close'].iloc[-1]:
-    api.submit_order(
-        symbol='AAPL',
-        qty=1,
-        side='buy',
-        type='market',
-        time_in_force='gtc'
-    )
-else:
-    api.submit_order(
-        symbol='AAPL',
-        qty=1,
-        side='sell',
-        type='market',
-        time_in_force='gtc'
-    )
+            model = train_model(X_train, y_train)
+            y_pred = model.predict(X_test)
+            mae = mean_absolute_error(y_test, y_pred)
+
+            print(f'Mean Absolute Error: {mae}')
+
+            X_last = X.iloc[-1]
+            last_close = df['Close'].iloc[-1]
+            predict_and_trade(model, X_last, last_close, symbol)
+
+        time.sleep(wait_time)
+
+if __name__ == "__main__":
+    main()
