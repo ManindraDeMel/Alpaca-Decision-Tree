f99c92ef62935c93bf109b724a133a142e75f16e
Manindra de Mel <u7156805@anu.edu.au>
2023-06-23 04:05:52 +1000

    Updated reward

--- trading_env.py
+++ trading_env.py
@@ -3,6 +3,7 @@ from gym import spaces
 import numpy as np
 import alpaca_trade_api as tradeapi
 import pandas as pd
+from utils import preprocess_data, calculate_technical_indicators  # import utility functions
 
 class TradingEnv(gym.Env):
     def __init__(self, alpaca_api_key, alpaca_secret_key, symbols):
@@ -22,6 +23,8 @@ class TradingEnv(gym.Env):
         self.current_step = 0
         self.get_data()
 
+        self.pnls = []
+
     def get_data(self):
         self.data = []
         for symbol in self.symbols:
@@ -30,6 +33,10 @@ class TradingEnv(gym.Env):
             self.data.append(bars)
         self.data = pd.concat(self.data, axis=1)
 
+        # Preprocess data and calculate technical indicators
+        self.data = preprocess_data(self.data)
+        self.data['rsi'] = calculate_technical_indicators(self.data)
+        
     def step(self, action):
         self.current_step += 1
 
@@ -52,11 +59,15 @@ class TradingEnv(gym.Env):
                     time_in_force='gtc'
                 )
 
-        # Calculate reward
-        # This is a simplified example where the reward is the change in portfolio value
-        # In a real-world application, you might want a more sophisticated reward function
+        # Calculate PnL
         portfolio = self.api.get_account()
-        reward = float(portfolio.portfolio_value) - float(portfolio.last_equity)
+        pnl = float(portfolio.portfolio_value) - float(portfolio.last_equity)
+        self.pnls.append(pnl)
+
+        # Calculate reward
+        avg_pnl = np.mean(self.pnls)
+        std_pnl = np.std(self.pnls)
+        reward = avg_pnl - std_pnl
 
         # Get new observation data
         self.get_data()
@@ -66,11 +77,13 @@ class TradingEnv(gym.Env):
 
         return observation, reward, done, {}
 
+
+
     def reset(self):
         self.current_step = 0
         self.get_data()
         observation = self.data.iloc[self.current_step].values
-
+        self.pnls = []
         return observation
 
     def render(self):
